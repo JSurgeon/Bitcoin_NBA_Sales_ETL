1940cf9b8197481ac19404212c295f1575209b0a
Gumball <jsurgeon29@gmail.com>
2021-06-10 21:27:16 -0700

    began to comment sections in jupyter notebook; created some schema in queries.sql file

---
+++ queries.sql
@@ -0,0 +1,23 @@
+CREATE TABLE features (
+id INT PRIMARY KEY,
+week DATE,
+store_id INT,
+temperature_far FLOAT,
+temperature_celcius FLOAT,
+fuel_price FLOAT,
+cpi FLOAT,
+is_holiday BOOLEAN,
+unemployment FLOAT,
+bitcoin_price FLOAT
+);
+
+CREATE TABLE sales (
+store_id INT,
+dept_num INT,
+week DATE,
+weekly_sales FLOAT,
+unnoficial_holiday VARCHAR
+);
+
+
+
