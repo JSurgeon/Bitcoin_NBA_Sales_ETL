614eea48771f3d26e67e0d9654535f441faaec09
Gumball <jsurgeon29@gmail.com>
2021-06-15 20:10:27 -0700

    updated main to upload only portland games; updated queries with primary keys

--- queries.sql
+++ queries.sql
@@ -1,4 +1,5 @@
 CREATE TABLE features (
+id SERIAL PRIMARY KEY,
 week INT,
 date DATE,
 store_id INT,
@@ -11,6 +12,7 @@ bitcoin_price FLOAT
 );
 
 CREATE TABLE sales (
+id SERIAL PRIMARY KEY,
 store INT,
 date DATE,
 dept INT,
@@ -20,7 +22,7 @@ unofficial_holiday VARCHAR
 
 CREATE TABLE games (
 date DATE,
-game_id INT,
+game_id INT PRIMARY KEY,
 home_team_id INT,
 home_team_name VARCHAR,
 visitor_team_id INT,
@@ -28,8 +30,25 @@ visitor_team_name VARCHAR
 );
 
 CREATE TABLE stores (
-store INT,
+store INT PRIMARY KEY,
 type CHAR,
 size INT
 );
 
+SELECT * FROM features;
+
+SELECT * FROM games;
+
+SELECT * FROM sales;
+
+SELECT * FROM stores;
+
+CREATE VIEW games_and_sales AS
+SELECT features.date, features.temperature_far, features.cpi, features.bitcoin_price,
+	sales.store, sales.dept, sales.weekly_sales, games.game_id, stores.type
+FROM features
+INNER JOIN sales ON features.date=sales.date
+INNER JOIN games ON games.date=sales.date
+INNER JOIN stores ON stores.store=features.store_id;
+
+select count(distinct(game_id)) from games_and_sales;
